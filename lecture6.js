a98bf18b377b0a1f97f9553fb6de7a744b81081c
Prince <princejaiswalpj251105@gmail.com>
2026-08-10 12:26:58 +0530

    L6

--- lecture6.js
+++ lecture6.js
@@ -1,28 +1,31 @@
-let score = "hitesh"
+let score = "33abc" //$
 
 console.log(typeof score);
-console.log(typeof(score));
+console.log(typeof(score)); // same as above
 
-let valueInNumber = Number(score)
+let valueInNumber = Number(score) // conversion in number 
 console.log(typeof valueInNumber);
 console.log(valueInNumber);
 
-
+//$
 // "33" => 33
-// "33abc" => NaN
-//  true => 1; false => 0
+// "33abc" => NaN(not a num)
+// null => 0
+// undefined => NaN
+// true => 1; false => 0
+// prince => NaN 
 
-let isLoggedIn = "hitesh"
+let isLoggedIn = "prince"
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
  console.log(booleanIsLoggedIn);
 
 // 1 => true; 0 => false
 // "" => false
-// "hitesh" => true
+// "prince" => true
 
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+console.log(stringNumber); // 33 
+console.log(typeof stringNumber); // string
